82510d039157d43eb898a587ea5c3fd6455395ed
SzramStaR <miloszkozlicki1@gmail.com>
2024-03-26 22:48:17 +0100

    Init

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.myapplication
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val szlak1: TextView = findViewById(R.id.szlak1)
+        szlak1.setOnClickListener{
+            val trailName = szlak1.text.toString()
+            Intent(this,Detail::class.java).also {
+                it.putExtra("EXTRA_trailName",trailName)
+                startActivity(it)
+            }
+        }
+    }
+}
